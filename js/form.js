70c3d6c9479569b59979e0c03388ed559d97127a
Lareta <lareta1@yandex.ru>
2018-03-29 19:16:27 +0300

    commit-6

---
+++ js/form.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+  $('.phone').mask("+7(999)999-9999", {autoclear: false});
+
+  $.validator.addMethod("checkMask", function(value, element) {
+       return this.optional(element) || /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value); 
+  }, "Введенный номер не корректен");
+
+// jQuery Validate JS
+  $("#contact-form").validate({
+    rules: {
+      name: { required: true },
+      email: { required: true, email: true },
+      // skype:  { required: true },
+      phone:{
+        required: true,
+        'checkMask': true
+      },
+      
+      message: { required: true }
+    },
+
+    /*messages: {
+      phone: {
+        required: "Пожалуйста, введите номер телефона",
+      }
+     // name: "Пожалуйста, введите свое имя",
+      //email: {
+       // required: "Пожалуйста, введите свой email",
+       //email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой."
+    },*/
+
+    submitHandler: function(form) {
+      ajaxFormSubmit();
+    }
+
+  })
+
+  // Функция AJAX запрса на сервер
+  function ajaxFormSubmit(){
+    var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
+
+    // Формируем ajax запрос
+    $.ajax({
+      type: "POST", // Тип запроса - POST
+      url: "php/mail.php", // Куда отправляем запрос
+      data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+      
+      // Функция если все прошло успешно
+      success: function(html){
+        $("#contact-form").slideUp(800);
+        $('#answer').html(html);
+        setTimeout(function() {$('#answer').hide();}, 3000);//закрываем окно благодарности
+        //$('#answer').show();//добавил Русик
+        setTimeout(function() {$('.overlay_popup').hide();}, 3000);
+      }
+    });
+
+    // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+    return false; 
+  };
+   // закрываем окно, нажимая на крестик
+   $('#form-close').click(function() {
+     $(".overlay_popup").hide();
+      $("#popup1").hide();
+   });
+      
+
+})
+
+    
--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
 
 	
-	/* Call plugins hrere ...*/
+	/* Call plugins hrere ...
+    //плавная прокрутка
+    $("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
+      highlightSelector:"nav a"
+  });*/
+
+		    //открываем поп 
+		$('.show_popup').click(function() { // Вызываем функцию по нажатию на кнопку 
+		    var popup_id = $('#' + $(this).attr("rel")); // Связываем rel и popup_id 
+		    $(popup_id).show(); // Открываем окно
+		    $('.overlay_popup').show(); // Открываем блок заднего фона
+		}) 
+		$('.overlay_popup').click(function() { // Обрабатываем клик по заднему фону
+		    $('.overlay_popup, .popup').hide(); // Скрываем затемнённый задний фон и основное всплывающее окно
+		});
+		//открываем поп 
+
+		/*animated
+		 wow = new WOW( {
+		     boxClass:     'wow',      // default
+		    animateClass: 'animated', // default
+		    offset:       100,          // через сколько пикселей сработает
+		    mobile:       false,       // default
+		    live:         true        // для мобильных оставить или убрать анимацию
+		  }
+		  )
+		  wow.init();
+		   new WOW().init();*/
 
 	
 });
